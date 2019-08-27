d60d745702ca4f0416f6617a45bb08d6e93751b4
CHOPPA2 <giahoangt30@gmail.com>
2019-08-27 12:18:20 +0700

    Fix to external scripts' import problem, change mainWindow's nodeIntegration to true.

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
-const {app, BrowserWindow, process, ipcRenderer, ipcMain} = require('electron')
+const {app, BrowserWindow, ipcRenderer, ipcMain} = require('electron')
 let mainWindow, htmlPath, secondWindow
+const process = require('process')
 const path = require('path')
 app.on('ready', () => {
   console.log('app is ready')
-  mainWindow = new BrowserWindow({width: 600  , height: 650})
+  mainWindow = new BrowserWindow({width: 600,
+    height: 650,
+    show: true,
+    resizeable: false,
+    webPreferences:
+    {nodeIntegration: true}  //extremely important as this makes main.js constants global for external scripts
+  }) //show false for best practices
   htmlPath = path.join(__dirname, 'index.html')
   mainWindow.loadFile(htmlPath)
   //mainWindow.webContents.openDevTools()
 })
 
-
+//show the app once all loaded
+//mainWindow.once('ready-to-show', () => {
+  //mainWindow.show()
+  //console.log('show')
+//})
 //quit app when close for Mac users
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') { // darwin is macOS win32 for Windows
