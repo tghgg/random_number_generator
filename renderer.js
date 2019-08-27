0789bccf004f000078a8388a87a9f678128e767b
CHOPPA2 <giahoangt30@gmail.com>
2019-08-27 12:26:24 +0700

    Fix refresh error by adding event.preventDefault()

--- renderer.js
+++ renderer.js
@@ -1,5 +1,6 @@
 //const {ipcRenderer} = require('electron')
 document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault()
   let random_number = Math.random()
   document.querySelector('#output').innerText = random_number
 })
